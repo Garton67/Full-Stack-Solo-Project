35702a2aed6a18ec920d378fc9fa00cc45890112
Garton67 <50034619+Garton67@users.noreply.github.com>
2022-01-18 21:28:45 -0600

    fixing folder issue

--- client/src/index.js
+++ client/src/index.js
@@ -23,6 +23,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
+ 
 
 
 ReactDOM.render(
